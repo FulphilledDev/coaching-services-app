2b3fc3ad6de7482176b7db23efd3fd5465036fb8
Philip Simpson <dev.phits@gmail.com>
2022-10-03 08:17:18 -0700

    Explore Page w/ Category Displays

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,8 +1,47 @@
+import { Link } from 'react-router-dom'
+import goForIt from '../assets/jpg/goForIt.jpg'
+import inLifting from '../assets/jpg/inLifting.jpg'
+import yesWeCan from '../assets/jpg/yesWeCan.jpg'
+
 function Explore() {
   return (
-    <>
-    <h1>Explore</h1>
-    </>
+    <div className='Explore'>
+      <header>
+        <p className="pageHeader">Explore</p>
+      </header>
+
+        <main>
+          {/* Slider */}
+          
+          <p className="exploreCategoryHeading">Categories</p>
+          <div className="exploreCategories">
+            <Link to='/category/life-performance'>
+              <img 
+                src={goForIt} 
+                alt="life-performance" 
+                className='exploreCategoryImg' 
+              />
+              <p className="exploreCategoryName">Life Performance</p>
+            </Link>
+            <Link to='/category/nutrition'>
+              <img 
+                src={inLifting} 
+                alt="nutrition" 
+                className='exploreCategoryImg' 
+              />
+              <p className="exploreCategoryName">Nutrition</p>
+            </Link>
+            <Link to='/category/mental-performance'>
+              <img 
+                src={yesWeCan} 
+                alt="mental-performance" 
+                className='exploreCategoryImg' 
+              />
+              <p className="exploreCategoryName">Mental Performance</p>
+            </Link>
+          </div>
+        </main>
+    </div>
   )
 }
 
