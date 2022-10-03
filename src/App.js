e85c970d820c8af33d25acb95023f6d6aebb28b0
Philip Simpson <dev.phits@gmail.com>
2022-10-02 18:25:42 -0700

    Assets, Navbar Component and Navigation links w/ class conditionals

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Explore from './pages/Explore';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+
+
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          {/*<Route path='/profile' element={<PrivateRoute />}> */}
+            <Route path='/profile' element={<SignIn />} />
+          {/* </Route> */}
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          {/* <Route path='/create-listing' element={<CreateListing />} /> */}
+        </Routes>
+        <Navbar />
+      </Router>
   );
 }
 
