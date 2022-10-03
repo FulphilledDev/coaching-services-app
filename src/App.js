754813a943e903a4fab267d681e139f41982132c
Philip Simpson <dev.phits@gmail.com>
2022-10-03 07:27:15 -0700

    Private Route Component & useAuthStatus hook

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 import Explore from './pages/Explore';
-import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
+import Profile from './pages/Profile';
+
 
 
 function App() {
@@ -15,13 +17,15 @@ function App() {
     <Router>
         <Routes>
           <Route path='/' element={<Explore />} />
-          {/*<Route path='/profile' element={<PrivateRoute />}> */}
+          <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
-          {/* </Route> */}
+          </Route>
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
-          {/* <Route path='/create-listing' element={<CreateListing />} /> */}
+          {/* <Route path='/create-listing' element={<PrivateRoute />}>
+            <Route path='/create-listing' element={<CreateListing />} />
+          </Route> */}
         </Routes>
         <Navbar />
       </Router>
