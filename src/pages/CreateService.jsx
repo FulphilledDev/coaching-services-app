2b594e55c04756da591132d9c0a9f95552e1d5e0
Philip Simpson <dev.phits@gmail.com>
2022-10-03 17:39:26 -0700

    Create Service Page: Form and functions

--- src/pages/CreateService.jsx
+++ src/pages/CreateService.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import Spinner from '../components/Spinner'
 
 function CreateService() {
-    const [ geoLocationEnabled, setGeoLocationEnabled ] = useState(true)
+    const [ geolocationEnabled, setGeolocationEnabled ] = useState(true)
     const [ loading, setLoading ] = useState(false)
 
     const [ formData, setFormData ] = useState({
@@ -15,7 +15,7 @@ function CreateService() {
         onlineCoaching: false,
         subscription: false,
         subscriptionPrice: 0,
-        minCommit: 0,
+        minCommit: 1,
         yearly: false,
         yearlyPrice: 0,
         avgRating: 3.5,
@@ -63,14 +63,286 @@ function CreateService() {
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isMounted])
 
-    if(loading) {
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        console.log(formData)
+    }
+
+    const onMutate = (e) => {
+    let boolean = null
+
+    if (e.target.value === 'true') {
+      boolean = true
+    }
+    if (e.target.value === 'false') {
+      boolean = false
+    }
+
+    // Files
+    if (e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        images: e.target.files,
+      }))
+    }
+
+    // Text/Booleans/Numbers
+    if (!e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: boolean ?? e.target.value,
+      }))
+    }
+  }
+
+  if(loading) {
         return <Spinner />
     }
 
 
-
   return (
-    <div>CreateService</div>
+    <div className='profile'>
+      <header>
+        <p className='pageHeader'>Create a Service</p>
+      </header>
+
+      <main>
+        <form onSubmit={onSubmit}>
+          <label className='formLabel'>Category</label>
+          <div className='formButtons'>
+            <button
+              type='button'
+              className={ category === 'mental-performance' ? 'formButtonActive' : 'formButton'}
+              id='category'
+              value='mental-performance'
+              onClick={onMutate}
+            >
+              Mental Performance
+            </button>
+            <button
+              type='button'
+              className={ category === 'nutrition' ? 'formButtonActive' : 'formButton'}
+              id='category'
+              value='nutrition'
+              onClick={onMutate}
+            >
+              Nutrition
+            </button>
+            <button
+              type='button'
+              className={ category === 'life-performance' ? 'formButtonActive' : 'formButton'}
+              id='category'
+              value='life-performance'
+              onClick={onMutate}
+            >
+              Life Performance
+            </button>
+          </div>
+
+        {/* make this autofill */}
+          <label className='formLabel'>Name</label>
+          <input
+            className='formInputName'
+            type='text'
+            id='name'
+            value={name}
+            onChange={onMutate}
+            maxLength='32'
+            minLength='5'
+            required
+          />
+
+          <label className='formLabel'>In-Person Coaching</label>
+          <div className='formButtons'>
+            <button
+              className={inPersonCoaching ? 'formButtonActive' : 'formButton'}
+              type='button'
+              id='inPersonCoaching'
+              value={true}
+              onClick={onMutate}
+            >
+              Yes
+            </button>
+            <button
+              className={
+                !inPersonCoaching && inPersonCoaching !== null ? 'formButtonActive' : 'formButton'
+              }
+              type='button'
+              id='inPersonCoaching'
+              value={false}
+              onClick={onMutate}
+            >
+              No
+            </button>
+          </div>
+
+          <label className='formLabel'>Online Coaching</label>
+          <div className='formButtons'>
+            <button
+              className={onlineCoaching ? 'formButtonActive' : 'formButton'}
+              type='button'
+              id='onlineCoaching'
+              value={true}
+              onClick={onMutate}
+            >
+              Yes
+            </button>
+            <button
+              className={
+                !onlineCoaching && onlineCoaching !== null
+                  ? 'formButtonActive'
+                  : 'formButton'
+              }
+              type='button'
+              id='onlineCoaching'
+              value={false}
+              onClick={onMutate}
+            >
+              No
+            </button>
+          </div>
+
+          {/* make this minimum commitment area */}
+          {/* <div className='formRooms flex'>
+            <div>
+              <label className='formLabel'>Bedrooms</label>
+              <input
+                className='formInputSmall'
+                type='number'
+                id='bedrooms'
+                value={bedrooms}
+                onChange={onMutate}
+                min='1'
+                max='50'
+                required
+              />
+            </div>
+            <div>
+              <label className='formLabel'>Bathrooms</label>
+              <input
+                className='formInputSmall'
+                type='number'
+                id='bathrooms'
+                value={bathrooms}
+                onChange={onMutate}
+                min='1'
+                max='50'
+                required
+              />
+            </div>
+          </div> */}
+
+
+          <label className='formLabel'>Business Location</label>
+          <textarea
+            className='formInputAddress'
+            type='text'
+            id='businessLocation'
+            value={businessLocation}
+            onChange={onMutate}
+          />
+
+          {!geolocationEnabled && (
+            <div className='formLatLng flex'>
+              <div>
+                <label className='formLabel'>Latitude</label>
+                <input
+                  className='formInputSmall'
+                  type='number'
+                  id='latitude'
+                  value={latitude}
+                  onChange={onMutate}
+                  required
+                />
+              </div>
+              <div>
+                <label className='formLabel'>Longitude</label>
+                <input
+                  className='formInputSmall'
+                  type='number'
+                  id='longitude'
+                  value={longitude}
+                  onChange={onMutate}
+                  required
+                />
+              </div>
+            </div>
+          )}
+
+          <label className='formLabel'>Yearly Payments</label>
+          <div className='formButtons'>
+            <button
+              className={yearly ? 'formButtonActive' : 'formButton'}
+              type='button'
+              id='yearly'
+              value={true}
+              onClick={onMutate}
+            >
+              Yes
+            </button>
+            <button
+              className={
+                !yearly && yearly !== null ? 'formButtonActive' : 'formButton'
+              }
+              type='button'
+              id='yearly'
+              value={false}
+              onClick={onMutate}
+            >
+              No
+            </button>
+          </div>
+
+          <label className='formLabel'>Subscription Price</label>
+          <div className='formPriceDiv'>
+            <input
+              className='formInputSmall'
+              type='number'
+              id='subscriptionPrice'
+              value={subscriptionPrice}
+              onChange={onMutate}
+              min='10'
+              max='10000'
+              required
+            />
+            {subscription === true && <p className='formPriceText'>$ / Month</p>}
+          </div>
+
+          {yearly && (
+            <>
+              <label className='formLabel'>Discounted Price</label>
+              <input
+                className='formInputSmall'
+                type='number'
+                id='yearlyPrice'
+                value={yearlyPrice}
+                onChange={onMutate}
+                min='10'
+                max='100000'
+                required={yearly}
+              />
+            </>
+          )}
+
+          <label className='formLabel'>Service Image</label>
+          <p className='imagesInfo'>
+            We suggest using the same Image as your Profile.
+          </p>
+          <input
+            className='formInputFile'
+            type='file'
+            id='images'
+            onChange={onMutate}
+            max='1'
+            accept='.jpg,.png,.jpeg'
+            required
+          />
+          <button type='submit' className='primaryButton createListingButton'>
+            Create Service
+          </button>
+        </form>
+      </main>
+    </div>
   )
 }
 
