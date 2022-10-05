92f6a658bf5cec17957dcbfe9c4603cd6460fdf5
Philip Simpson <dev.phits@gmail.com>
2022-10-05 11:28:37 -0700

    Service Item TouchUps

--- src/components/ServiceItem.jsx
+++ src/components/ServiceItem.jsx
@@ -9,44 +9,54 @@ import starFillIcon from '../assets/svg/starFillIcon.svg'
 function ServiceItem({ service, id, onDelete, onEdit }) {
   return (
     <li className='categoryListing'>
-      <Link to={`/category/${service.category}/${id}`} className='categoryListingLink'>
-        <img src={service.imgUrls[0]} alt={service.name} className='categoryListingImg' />
-        <div className="categoryListingDetails">
-          <p className="categoryListingLocation">
-              {service.businessLocation}
-          </p>
-          <p className="categoryListingName">
-              {service.name}
-          </p>
-          <p className="categoryListingPrice">
-                {service.yearly 
-                    ? service.yearlyPrice
-                        .toString()
-                        .padStart(service.yearlyPrice.length+1, '$')
-                        .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-                        .concat(' / Year')
-                    : <></> }
-                { service.yearly && service.subscription ? ' or ' : <></>}
-                {service.subscription 
-                    ? service.subscriptionPrice
-                        .toString()
-                        .padStart(service.subscriptionPrice.length+1, '$')
-                        .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-                        .concat(' / Month')
-                    : <></> }
-          </p>
-          {/* Add conditional for "avgRating" and "numberOfClients" (still need to add to service profile, database, Review form for clients? (Maybe just comment section with option to select stars), etc) */}
-          <div className="categoryListingInfoDiv">
-              <img src={starFillIcon} alt="star rating" className='ratingImg'/>
-              <img src={starFillIcon} alt="star rating" className='ratingImg'/>
-              <img src={starFillIcon} alt="star rating" className='ratingImg'/>
-              <img src={starHalfIcon} alt="star rating" className='ratingImg'/>
-              <p className="categoryListingInfoText">
-                  {service.minCommit > 1 ? `${service.minCommit} reviews` : 'No Ratings Yet'}
-              </p>
-          </div>
-        </div>
-      </Link>
+        <Link to={`/category/${service.category}/${id}`} className='categoryListingLink'>
+            <img src={service.imgUrls[0]} alt={service.name} className='categoryListingImg' />
+            <div className="categoryListingDetails">
+                <p className="categoryListingLocation">
+                    {service.businessLocation}
+                </p>
+                <p className="categoryListingName">
+                    {service.name}
+                </p>
+                <p className="categoryListingPrice">
+                    {service.yearly 
+                        ? service.yearlyPrice
+                            .toString()
+                            .padStart(service.yearlyPrice.length+1, '$')
+                            .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                            .concat(' / Year')
+                        : <></> }
+                    { service.yearly && service.subscription ? ' or ' : <></>}
+                    {service.subscription 
+                        ? service.subscriptionPrice
+                            .toString()
+                            .padStart(service.subscriptionPrice.length+1, '$')
+                            .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                            .concat(' / Month')
+                        : <></> }
+                </p>
+                {/* Add conditional for "avgRating" and "numberOfClients" (still need to add to service profile, database, Review form for clients? (Maybe just comment section with option to select stars), etc) */}
+                <div className="categoryListingInfoDiv">
+                    <div className="categoryListingInfoText">
+                        {service.minCommit === 0 
+                            ? 'No Ratings Yet' 
+                            : <>
+                                <p className='starRatings'>
+                                    <img src={starFillIcon} alt="star rating" className='starIcon starIconOne'/>
+                                    <img src={starFillIcon} alt="star rating" className='starIcon'/>
+                                    <img src={starFillIcon} alt="star rating" className='starIcon'/>
+                                    <img src={starHalfIcon} alt="star rating" className='starIcon'/>
+                                    <span className='starReviews'>
+                                        {service.minCommit > 1 ? `${service.minCommit} Reviews` : '1 Review' } 
+                                    </span>
+                                </p>
+                                 
+                            </>
+                        }
+                    </div>
+                </div>
+            </div>
+        </Link>
 
       {onDelete && (
             <DeleteIcon 
